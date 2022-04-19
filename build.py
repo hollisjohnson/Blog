900c656ae3361e253998f1efb32f7beea1197040
Holly Johnson <hollyann026@gmail.com>
2022-04-19 07:16:36 -0700

    build.py

---
+++ build.py
@@ -0,0 +1,164 @@
+#make 7-amazing.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/7-amazing.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/7-amazing.html' , 'w+').write(combined_html)
+
+#make a-new-start.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/a-new-start.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/a-new-start.html' , 'w+').write(combined_html)
+
+#make about.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/about.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/about.html' , 'w+').write(combined_html)
+
+#make blog.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/blog.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/blog.html' , 'w+').write(combined_html)
+
+#make contact.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/contact.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/contact.html' , 'w+').write(combined_html)
+
+#make developers-of-color.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/developers-of-color.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/developers-of-color.html' , 'w+').write(combined_html)
+
+#make easier-way.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/easier-way.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/easier-way.html' , 'w+').write(combined_html)
+
+#make index.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/index.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/index.html' , 'w+').write(combined_html)
+
+#make machine-learning.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/machine-learning.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/machine-learning.html' , 'w+').write(combined_html)
+
+#make my-first.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/my-first.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/my-first.html' , 'w+').write(combined_html)
+
+#make passive-income.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/passive-income.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/passive-income.html' , 'w+').write(combined_html)
+
+#make projects.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/projects.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/projects.html' , 'w+').write(combined_html)
+
+#make recruiting.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/recruiting.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/recruiting.html' , 'w+').write(combined_html)
+
+#make tapping-into.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/tapping-into.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/tapping-into.html' , 'w+').write(combined_html)
+
+#make wireframes.html in docs/
+top_html = open('templates/top.html').read()
+
+bottom_html = open('templates/bottom.html').read()
+
+content_html = open('content/wireframes.html').read()
+
+combined_html = top_html + content_html + bottom_html
+
+open('docs/wireframes.html' , 'w+').write(combined_html)
